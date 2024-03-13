ba2e7de428e59597d1af02b1c9b5ba1fadb3c26b
Durgesh Mishra <durgeshmishra6099@gmail.com>
2024-03-13 20:50:04 +0530

    14 Commit

--- src/components/head.js
+++ src/components/head.js
@@ -46,10 +46,10 @@ const Head = ({ title, description, image }) => {
 
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
-
+      {/*Done At All*/}
       <meta property="og:title" content={seo.title} />
       <meta property="og:description" content={seo.description} />
-      <meta property="og:image" content={seo.og} />
+      {/* <meta property="og:image" content={seo.og} /> */}
       <meta property="og:url" content={seo.url} />
       <meta property="og:type" content="website" />
 
